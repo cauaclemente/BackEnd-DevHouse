f5ee16da60a0e18043f4f8544e4027fa33bf64c2
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-11 20:58:10 -0300

    ajustes no mongoDB

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
 import routes from "./routes";
 
 class App{
 
     constructor(){
         this.server = express();
+
+        mongoose.connect("mongodb+srv://devhouse:devhouse123@devhouse.6db7skr.mongodb.net/devhouse?retryWrites=true&w=majority&appName=DevHouse");
 
         this.middleware();
         this.routes();
