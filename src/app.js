3e40d09d1aa83265353fc9075b4ecab8a5d6a46a
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-12 16:28:18 -0300

    cadastrando casas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path"
+
 import routes from "./routes";
 
 class App{
@@ -14,6 +16,12 @@ class App{
     }
 
     middleware(){
+
+        this.server.use(
+            "/files",
+            express.static(path.resolve(__dirname, "..", "uploads"))
+        )
+
         this.server.use(express.json())
     }
 
